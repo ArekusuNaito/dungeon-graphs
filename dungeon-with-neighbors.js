9c9f36df6a741e0c4e87508e1ad747a9faf01956
ArekusuNaito <arekusunaito@gmail.com>
2018-05-09 15:50:25 -0400

    Algorithm now solves the dungeon with connected rooms. Still no optional
    Added that magic line of ...
    "If you only have one neighbor and you came from it..." Means that you are allowed to go back!

--- dungeon-with-neighbors.js
+++ dungeon-with-neighbors.js
@@ -9,63 +9,80 @@ let inventory = [];
 //Start node actions
 dungeon.Start.name = "Start";
 open["Start"] = dungeon.Start;
-let counter = 0;
+let deadEndCounter = 0;
 let currentRoom = dungeon.Start;
 currentRoom.name = "Start";
 currentRoom.comingFrom = null;
-let doorIndex = 0;
 VisitRoom(currentRoom);
 while(closed.End)
 {
-  CheckDoor(currentRoom,doorIndex);
+  for(let index in currentRoom.neighbors)
+  {
+    let neighborName = currentRoom.neighbors[index];
+    let neighbor = GetRoomData(neighborName);
+    if(currentRoom.comingFrom == neighbor && currentRoom.neighbors.length>1)
+    {
+      console.log(`That's where I come from... ${neighbor.name}`);
+      deadEndCounter++;
+      if(deadEndCounter>=30) //If you reached a lot of dead ends, end the exploration
+      {
+        EndExploration();
+        process.exit();
+      }
+      continue;
+    }
+    DoICarryTheRequirement(neighbor,(requirementIndex,inventoryIndex)=>
+    {
+      UseItem(neighbor,inventoryIndex,requirementIndex);
+    });
+    if(CanYouOpen(neighbor))
+    {
+
+      VisitRoom(neighbor);
+      neighbor.comingFrom = currentRoom;
+      currentRoom=neighbor;
+      break;
+    }
+    else
+    {
+      console.log(`I can't open ${neighbor.name}`);
+    }
+  }
 }
 //
-console.log();
-console.log(`Unvisited Rooms: ${Object.keys(closed).length}`);
-console.log(`Your loot: ${inventory}`);
-console.log(path);
-console.log();
+EndExploration();
 // path.map(room=>console.log(room.name))
 
 ////----------- Functions
 
-function CheckDoor(currentRoom,doorIndex)
+function EndExploration()
 {
   console.log();
-  let neighborName = currentRoom.neighbors[doorIndex];
-  if(!neighborName)
-  {
-    console.log(`Where... am I...? I came from ${currentRoom.comingFrom}`);
-    // process.exit();
-    return;
-  }
-  let neighbor = GetRoomData(neighborName)
-  if(neighbor.comingFrom == currentRoom.name)
-  {
-    console.log("That's where I came from...");
-    CheckDoor(currentRoom,doorIndex+1)
-  }
-  console.log(`You are looking at the door of ${neighborName}`);
-  //Can we enter that room?
+  console.log(`Unvisited Rooms: ${Object.keys(closed).length}`);
+  console.log(`Your loot: ${inventory}`);
+  console.log(path);
+  console.log();
+}
 
-  if(RoomRequirementsMet(neighbor))
+function DoICarryTheRequirement(room,action)
+{
+  if(room.requirements)
   {
-    neighbor.comingFrom = currentRoom.name;
-    VisitRoom(neighbor);
-    CheckDoor(neighbor,doorIndex)
+    for(let requirementIndex in room.requirements)
+    {
+      let requirement = room.requirements[requirementIndex];
+      if(inventory.includes(requirement))
+      {
+        let inventoryIndex = inventory.indexOf(requirement);
+        action(requirementIndex,inventoryIndex);
+      }
+    }
   }
-  else
-  {
-    console.log(`${neighborName} its locked... Requires ${neighbor.requirements}`);
-    CheckDoor(currentRoom,doorIndex+1)
-  }
-  console.log("When are we supposed to see this?");
 }
 
 
 function GetRoomData(roomName)
 {
-  console.log(roomName);
   let room = open[roomName] || closed[roomName]
   room.name = roomName;
   return room;
@@ -75,17 +92,14 @@ function VisitRoom(room)
 {
 
   console.log(`Entering ${room.name}`);
+  open[room.name]=room;
   path.push(room.name);
   if(RoomAwardSomething(room))
   {
-    console.log(`${room.name} awards ${room.awards}`);
     room.awards.map(item=>inventory.push(item));
+    console.log(`${room.name} awards ${room.awards}. Your inventory:[${inventory}]`);
     delete room.awards;
 
-  }
-  else
-  {
-    //Message: Nothing to award, kbyethx
   }
   if(!room.awards && !room.unlocks)
   { //No more useful stuff here
@@ -99,35 +113,20 @@ function RoomAwardSomething(room)
   return room.awards
 }
 
-function RoomRequirementsMet(room)
+function UseItem(room,inventoryIndex,requirementIndex)
 {
-  //Check for a Key
-  if(room.requirements)
-  {
-    for(let index in room.requirements)
-    {
-      let requirement = room.requirements[index];
-      if(inventory.includes(requirement))
-      {
-        console.log(`To Open: ${room.requirements} and You have ${inventory}`);
-        let itemIndex = inventory.indexOf(requirement);
-        inventory.splice(itemIndex,1);
-        room.requirements.splice(index,1);
-        console.log("Used a key to open the door");
-        console.log(`And now requires: ${room.requirements} and You have ${inventory}`);
-      }
-    }
+  let item = inventory[inventoryIndex];
+  console.log(`Using ${item} to clear a requirement of ${room.name}`);
+  inventory.splice(inventoryIndex,1);
+  room.requirements.splice(requirementIndex,1);
+}
 
-  }
-  if(room.name == "Locked-Door")
-  {
-    // process.exit();
-  }
+function CanYouOpen(room)
+{
+
   if(!room.requirements || room.requirements.length===0)
   {
-    delete room.requirements;
     console.log(`You are allowed to enter ${room.name}`);
-    open[room.name]=room;
     return true;
   }
   else
